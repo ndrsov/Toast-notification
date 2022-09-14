c9fb439f416473c1bf0b04c2176fa5b0d9d24829
Andres Soria <andres.zov@gmail.com>
2022-09-14 14:02:42 -0500

    Add fucntion to create a random message end type

--- scritp.js
+++ scritp.js
@@ -9,16 +9,26 @@ const messages = [
   "Messagge five",
 ];
 
-button.addEventListener("click", createNotification);
+const types = ["info", "success", "error"];
 
-function createNotification() {
+button.addEventListener("click", () => createNotification());
+
+function createNotification(message = null, type = null) {
   const notif = document.createElement("div");
   notif.classList.add("toast");
-  notif.innerText = getRandomMessage();
+  notif.classList.add(type ? type : getRandomType());
+  notif.innerText = message ? message : getRandomMessage();
 
   toasts.appendChild(notif);
+
+  setTimeout(() => {
+    notif.remove();
+  }, 3000);
 }
 
 function getRandomMessage() {
   return messages[Math.floor(Math.random() * messages.length)];
+}
+function getRandomType() {
+  return types[Math.floor(Math.random() * types.length)];
 }
